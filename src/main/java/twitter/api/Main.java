e3df5e29f6a5136ab49bf4a1d084c9733c99a7f7
justi <justinwynkoop@gmail.com>
2018-05-15 22:24:00 -0400

    Added GUI

--- src/main/java/twitter/api/Main.java
+++ src/main/java/twitter/api/Main.java
@@ -2,11 +2,16 @@ package twitter.api;
 
 import twitter4j.TwitterException;
 
+
+
 public class Main {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        SimplerTwitterGUI gui = new SimplerTwitterGUI();
         TwitterLogin twitterLogin = new TwitterLogin();
-        System.out.println("Hello");
+        //System.out.println("Hello");
 
         /*TwitterApiConnection twitterApiConnection = new TwitterApiConnection();
         try {
@@ -16,5 +21,6 @@ public class Main {
             e.printStackTrace();
         }*/
     }
-
 }
+
+
--- src/main/java/twitter/api/SimplerTwitterGUI.java
+++ src/main/java/twitter/api/SimplerTwitterGUI.java
@@ -1,4 +1,14 @@
 package twitter.api;
 
-public class SimplerTwitterGUI {
+import javax.swing.*;
+
+public class SimplerTwitterGUI extends JFrame {
+
+    public SimplerTwitterGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000,1000);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
 }
